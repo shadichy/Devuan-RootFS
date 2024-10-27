e71659139891c3c51232439823f5283bc3c78c28
hmtheboy154 <buingoc67@gmail.com>
2024-10-28 02:45:08 +0700

    Revert "Try to strip out setup.sh"
    This reverts commit 2fee2b637ac08ab262b1c45296c26d9fb2e88571.

--- setup.sh
+++ setup.sh
@@ -17,6 +17,17 @@ done
 
 echo 'blissos.org' > /etc/hostname
 
+cat <<EOF > /usr/sbin/autologin
+#!/bin/sh
+exec login -f root
+EOF
+chmod +x  usr/sbin/autologin
+sed -i 's@1:2345:respawn:/sbin/getty@1:2345:respawn:/sbin/getty -n -l /usr/sbin/autologin@g' /etc/inittab
+sed -i -r 's|^(root:.*:)/bin/d?a?sh$|\1/bin/bash|g' /etc/passwd
+
+# shellcheck disable=SC2016
+echo 'udevadm trigger && [ -z "$DISPLAY" ] && { startx /usr/bin/jwm; poweroff; }' > /root/.bash_profile
+chmod +x  /root/.bash_profile
 
 for d in libselinux.so.1 libc.so.6 ld-linux-x86-64.so.2; do
   ln -s x86_64-linux-gnu/$d /lib/
