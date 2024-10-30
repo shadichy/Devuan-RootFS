723e93b0194ed301afdafc9513edde1cd9060a00
shadichy <hotrobb360web@gmail.com>
2024-10-31 00:08:16 +0700

    strip setup.sh by creating needed files in template; use patched calamares repository

--- template/etc/bliss/reboot.sh
+++ template/etc/bliss/reboot.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 gxmessage -nearmouse -buttons "Yes:3,No:4" -name "Restart" "Reboot?"
 case $? in
-	3) 
-	/sbin/reboot
-	;;
-	
-	*)  echo "abort Reboot"
-	;;
+3) /sbin/reboot ;;
+*) echo "abort Reboot" ;;
 esac
