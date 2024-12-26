6248493994a325aea72c170861ad785b993dfd44
shadichy <hotrobb360web@gmail.com>
2024-12-26 23:28:33 +0700

    Upstream push: - Remove debug tools - Rename initrd lib archive - Change apt repo back to Bliss - Modify calamares launcher to use Gtk3 theme - Trim down Raleigh gtk theme
    Signed-off-by: shadichy <hotrobb360web@gmail.com>

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -5,25 +5,25 @@ grub-mkrescue -o /grub-rescue.iso /iso
 rm -rf /iso
 
 # Generate initrd template
-mkdir -p /initrd/usr/{bin,lib,lib64}
+mkdir -p /initrd_lib/usr/{bin,lib,lib64}
 ln -st /initrd usr/{bin,lib,lib64}
 
 # Copy binaries and libraries
 find_dep() { ldd "$1" | awk '{print $3}' | xargs; }
 for b in mount.ntfs-3g; do
   b=$(which $b)
-  cp -t /initrd/bin $b
-  cp -t /initrd/lib $(find_dep "$b")
+  cp -t /initrd_lib/bin $b
+  cp -t /initrd_lib/lib $(find_dep "$b")
 done
 
 # Busybox is explicitly handled
-cp -t /initrd/bin /usr/share/bliss/busybox
-cp -t /initrd/lib $(find_dep /initrd/bin/busybox)
+cp -t /initrd_lib/bin /usr/share/bliss/busybox
+cp -t /initrd_lib/lib $(find_dep /initrd_lib/bin/busybox)
 
 # Linker
-cp -t /initrd/bin /bin/ld.so
-cp -t /initrd/lib /usr/lib/*/ld-linux-x86-64.so.*
-cp -t /initrd/lib64 /usr/lib64/ld-linux-x86-64.so.*
+cp -t /initrd_lib/bin /bin/ld.so
+cp -t /initrd_lib/lib /usr/lib/*/ld-linux-x86-64.so.*
+cp -t /initrd_lib/lib64 /usr/lib64/ld-linux-x86-64.so.*
 
 # Wrap initrd up
 tar -czvf /initrd_lib.tar.gz /initrd
